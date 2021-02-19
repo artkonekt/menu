1419a633e47620209027481f992b1d10dbf19dc3
Attila Fulop <1162360+fulopattila122@users.noreply.github.com>
2021-02-19 11:35:42 +0100

    Added docblock annotations to menu facade

--- src/Facades/Menu.php
+++ src/Facades/Menu.php
@@ -9,14 +9,18 @@
  *
  */
 
-
 namespace Konekt\Menu\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static create($name, $options = []): \Konekt\Menu\Menu
+ * @method static get($name): \Konekt\Menu\Menu|null
+ * @method static all(): \Illuminate\Support\Collection
+ *
+ */
 class Menu extends Facade
 {
-
     /**
      * Get the registered name of the component.
      *
